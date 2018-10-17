cf119509e571721f2a42c2ca5770d1af05e4097b
Tomas Cejka <tomas.cejka@strv.com>
2018-10-17 19:19:12 +0200

    First two task solution

--- AVFWorkshop/ViewControllers/PreviewViewController.swift
+++ AVFWorkshop/ViewControllers/PreviewViewController.swift
@@ -94,7 +94,39 @@ private extension PreviewViewController {
      */
 
     func showAssetMetadata() {
+        // proper solution, not to block thread
+        asset.loadValuesAsynchronously(forKeys: requiredAssetKeys) { [weak self] in
+            guard let `self` = self else { return }
 
+            // check if value loaded properly
+            var error: NSError? = nil
+            for key in self.requiredAssetKeys {
+                // guard each value if loaded
+                guard self.asset.statusOfValue(forKey: key, error: &error) == .loaded else {
+                    if error != nil {
+                        // log anything is wrong
+                        print(error?.localizedDescription ?? "error loading asset keys")
+                    }
+                    return
+                }
+            }
+
+            // need to be called on main bc touching ui
+            // careful blocking thread if not loaded before
+            DispatchQueue.main.async { [weak self] in
+                guard let `self` = self else { return }
+                let metadata = """
+                LOADED ASSET METADATA: \n
+                duration: \(self.asset.duration) \n
+                isPlayable: \(self.asset.isPlayable) \n
+                metadata: \(self.asset.metadata) \n
+                tracks: \(self.asset.tracks) \n
+                """
+
+                // set prepared text
+                self.metadataTextView.text = metadata
+            }
+        }
     }
 }
 
